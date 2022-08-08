4d383d3e1ce250de4e7fd05e24721b5ef1bead55
Vishal R <67860986+Mrvishal2k2@users.noreply.github.com>
2022-08-08 20:54:43 +0530

    Create commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,8 +1,5 @@
 import os
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+from config import Config
 from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
